3dbecfb40f257b3842d10b8d5a72a820b2a45ae5
perdidev <perdidev@gmail.com>
2024-08-20 22:18:14 +0800

    fix: add component in user and map

--- features/map/map.tsx
+++ features/map/map.tsx
@@ -1,12 +1,19 @@
 import Image from "next/image";
+import { FaChevronDown } from "react-icons/fa6";
 
 export const Map = () => {
   return (
     <div className="p-12">
       <div className="flex justify-between">
         <div className="flex gap-4">
-          <button className="h-fit rounded-md bg-white px-2 py-1 text-sm font-semibold">
-            Drone-A01
+          <button className="group relative z-50 flex w-max items-center gap-2 overflow-hidden rounded-lg border border-zinc-200 bg-white px-2 py-1 text-sm font-semibold transition-all duration-200 focus:overflow-visible">
+            <span>Drone-A01</span>
+            <FaChevronDown className="-rotate-90 group-focus:rotate-0" />
+            <div className="absolute -bottom-12 left-0 flex h-max w-full flex-col gap-2 rounded-lg border border-zinc-200 bg-white p-1 shadow-lg">
+              <span className="flex flex-row items-center gap-2 rounded-lg p-2 hover:bg-zinc-100">
+                <p>Drone-A02</p>
+              </span>
+            </div>
           </button>
           <div className="h-fit rounded-md bg-emerald-400/25 px-2 py-1 text-sm font-medium">
             Active
