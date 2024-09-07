bf2345ec7a60d57cd565121284d7e96237920fea
unknown <nadtss12@gmail.com>
2024-09-07 13:43:01 +0800

    up active sidebar

--- components/adminSidebar/Sidebar.tsx
+++ components/adminSidebar/Sidebar.tsx
@@ -4,9 +4,12 @@ import { navlink } from "./navLink";
 import Link from "next/link";
 import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export const SideBar = () => {
   const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+  const pathname = usePathname();
+
   const handleToggle = (index: number) => {
     setExpandedIndex(expandedIndex === index ? null : index);
   };
@@ -17,56 +20,78 @@ export const SideBar = () => {
         <Image src="/svg/logo.svg" width={100} height={100} alt="" />
       </div>
       <section className="flex flex-col gap-6 py-10">
-        {navlink.map((item, index) => (
-          <div key={index} className="flex flex-col">
-            <div className="flex w-full flex-col justify-center pl-6">
-              <button
-                className="flex items-center gap-4 rounded-md py-2 pl-6 pr-10 duration-200 ease-in-out hover:bg-blue-900"
-                onClick={() => handleToggle(index)}
-              >
-                <Link
-                  href={item.href}
-                  className="flex w-full items-center gap-4"
+        {navlink.map((item, index) => {
+          const isActive = pathname === item.href;
+
+          return (
+            <div key={index} className="flex flex-col">
+              <div className="flex w-full flex-col justify-center pl-6">
+                <button
+                  className={`flex items-center gap-4 rounded-md py-2 pl-6 pr-10 duration-200 ease-in-out ${isActive ? "rounded-none bg-white text-primary-darkblue" : "hover:bg-blue-900"}`}
+                  onClick={() => handleToggle(index)}
                 >
-                  {item.icon && <item.icon className="h-4 w-4 text-white" />}
-
-                  <span className="text-white">{item.title}</span>
-                </Link>
-                {item.sublink && (
-                  <div className="text-white">
-                    {expandedIndex === index ? (
-                      <AiOutlineUp className="h-4 w-4" />
-                    ) : (
-                      <AiOutlineDown className="h-4 w-4" />
+                  <Link
+                    href={item.href}
+                    className="flex w-full items-center gap-4"
+                  >
+                    {item.icon && (
+                      <item.icon
+                        className={`h-4 w-4 ${isActive ? "text-primary-darkblue" : "text-white"}`}
+                      />
                     )}
-                  </div>
-                )}
-              </button>
-
-              {item.sublink && expandedIndex === index && (
-                <div className="flex transform flex-col gap-4 pl-6 pt-4 text-white duration-300 ease-in-out">
-                  {item.sublink.map((subitem, subindex) => (
-                    <div
-                      key={subindex}
-                      className="flex items-center gap-4 rounded-md py-2 pl-8 duration-200 ease-in-out hover:bg-blue-900"
+                    <span
+                      className={`text-base ${isActive ? "text-primary-darkblue" : "text-white"}`}
                     >
-                      <Link href={subitem.href}>
-                        {subitem.icon && <subitem.icon className="h-4 w-4" />}
-                      </Link>
-                      {subitem.href ? (
-                        <Link href={subitem.href} className="flex flex-col">
-                          {subitem.title}
-                        </Link>
+                      {item.title}
+                    </span>
+                  </Link>
+                  {item.sublink && (
+                    <div className="text-white">
+                      {expandedIndex === index ? (
+                        <AiOutlineUp className="h-4 w-4" />
                       ) : (
-                        ""
+                        <AiOutlineDown className="h-4 w-4" />
                       )}
                     </div>
-                  ))}
-                </div>
-              )}
+                  )}
+                </button>
+
+                {item.sublink && expandedIndex === index && (
+                  <div className="flex transform flex-col gap-4 pl-6 pt-4 text-white duration-300 ease-in-out">
+                    {item.sublink.map((subitem, subindex) => {
+                      const isSubitemActive = pathname === subitem.href;
+
+                      return (
+                        <div
+                          key={subindex}
+                          className={`flex items-center gap-4 rounded-md py-2 pl-8 duration-200 ease-in-out ${isSubitemActive ? "rounded-none bg-white text-primary-darkblue" : "hover:bg-blue-900"}`}
+                        >
+                          <Link href={subitem.href}>
+                            {subitem.icon && (
+                              <subitem.icon
+                                className={`h-4 w-4 ${isSubitemActive ? "text-primary-darkblue" : "text-white"}`}
+                              />
+                            )}
+                          </Link>
+                          {subitem.href ? (
+                            <Link
+                              href={subitem.href}
+                              className={`flex flex-col ${isSubitemActive ? "text-primary-darkblue" : "text-white"}`}
+                            >
+                              {subitem.title}
+                            </Link>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </section>
     </section>
   );
